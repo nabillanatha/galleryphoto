e418cda94570864b49929223e5d5b3e2a8e8a1a7
kelompok 5 <a8pratiwi@gmail.com>
2024-03-06 10:56:41 +0700

    first commit

---
+++ database/migrations/2024_03_06_032718_create_gallery_images_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gallery_images', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedInteger('user_id');
+            $table->string('caption');
+            $table->string('category');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gallery_images');
+    }
+};
